28fce24c6f7128995e6a3938316a070ce692c60b
mert-devops <44655905+mert-devops@users.noreply.github.com>
2018-11-07 08:39:13 +1100

    Update 4-buildndeploy.ps1
    added missing comments

--- 4-buildndeploy.ps1
+++ 4-buildndeploy.ps1
@@ -13,20 +13,26 @@ $imagenameandversion = $JsonObject[0].imagenameandversion;
 gcloud config set project $projectname
 gcloud config set compute/zone $gcezone
 
+#creating docker image and tagging before pushing it to gcloud projects container registery
 Set-Location .\App\
 
 docker build . -t gcr.io/$projectname/$imagenameandversion
 
 Set-Location ..\
 
+#request a token and pass it to docker for logging in to the projects container registery
 $token=gcloud auth print-access-token
 docker login -u oauth2accesstoken -p $token https://gcr.io
 
+#pushing (uploading) image to registery so our kubernetes deployment can use this image in container spec
 docker push gcr.io/$projectname/$imagenameandversion
 
+#request credentials for kubernetes cluster, updates local config so kubectl can authenticate
 gcloud container clusters get-credentials $gkeclustername
 
+#create kubernetes deployment via config file
 kubectl create -f techtestapp.yml
 
+#create a service with type load balancer in order to expose port 80 to our deployment tagged pods provides external HTTP address
 kubectl expose deployment techtestapp-deployment --type=LoadBalancer --port 80 --target-port 80
 
