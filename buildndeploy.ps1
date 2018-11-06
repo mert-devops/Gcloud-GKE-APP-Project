80df84f4994b9e9e93da80d50ffcc6aa49e4fc83
mert-devops <44655905+mert-devops@users.noreply.github.com>
2018-11-06 15:19:01 +1100

    created a json file to contain all config in one place
    1- So that multiple scripts can use if needed
    2- It would be easier for assessor to change values for his/her run of the project

--- buildndeploy.ps1
+++ buildndeploy.ps1
@@ -1,1 +1,11 @@
+#This script assumes you have your Gcloud environment initiated 
+#This script needs to be ran after you have generated neccesary gcloud infrastruce and enabled APIs neccesary for this project. 
+
+$JsonObject = Get-Content .\projectconfig.json | ConvertFrom-Json
+
+
+$projectname = $JsonObject[0].projectname;
+
 Set-Location .\App\
+
+
