9277230c6359fc569143c4fd5aa1e012e9e0f7e6
mert-devops <44655905+mert-devops@users.noreply.github.com>
2018-11-06 15:40:34 +1100

    project conf.toml is edited to adopt Gcloud Cloud SQL Proxy
    conforming to the offical gcloud documuenetion
    https://cloud.google.com/sql/docs/postgres/connect-kubernetes-engine

--- buildndeploy.ps1
+++ buildndeploy.ps1
@@ -1,11 +1,15 @@
 #This script assumes you have your Gcloud environment initiated 
 #This script needs to be ran after you have generated neccesary gcloud infrastruce and enabled APIs neccesary for this project. 
 
-$JsonObject = Get-Content .\projectconfig.json | ConvertFrom-Json
-
+$JsonObject = Get-Content  .\projectconfig.json | ConvertFrom-Json
 
 $projectname = $JsonObject[0].projectname;
 
+
+
 Set-Location .\App\
 
+docker build . -t gcr.io/$projectname/techtestapp:v1
 
+$token=gcloud auth print-access-token
+docker login -u oauth2accesstoken -p $token https://gcr.io
